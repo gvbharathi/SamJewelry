ea7998e164802174ed8febd3df01be5d77d773d3
gvbharathi <gvbharathi@hotmail.com>
2016-04-07 22:46:54 -0700

    I added  addto cart and view cart

--- SamJewelry/Jewelry.cs
+++ SamJewelry/Jewelry.cs
@@ -37,10 +37,42 @@ namespace SamJewelry
         public string JeWelryName { get; set; }
         public Jewelrytype Typeofjewelry { get; internal set; }
         //public JewelryType TypeofJewelry { get; set; }
-
         #endregion
+        #region methods
+        public void AddNecklacestocart(int numberofnecklaces)
+        {
+            Necklaces += numberofnecklaces;
+        }
 
+        public void AddEarringstocart(int numberofearrings)
+        {
+            Earrings += numberofearrings;
+        }
 
+        public void AddPendentstocart(int numberofpendents)
+        {
+            Pendents += numberofpendents;
+        }
+        public void AddBanglestocart(int numberofBangles)
+        {
+            Bangles += numberofBangles;
+        }
+        public void AddLongchainstocart(int numberofLongchains)
+        {
+            Longchains +=numberofLongchains;
+        }
+            #endregion
+
+         public void viewcart()
+        {
+            Console.WriteLine("No of Necklaces:  " + Necklaces);
+            Console.WriteLine("No of Earings:  " + Earrings);
+            Console.WriteLine("No of Pendents:  " + Pendents);
+            Console.WriteLine("No of Bangles:  " + Bangles);
+            Console.WriteLine("No of Longchains:  " + Longchains); 
+        }
     }
 
-   }
+  }
+
+   
--- SamJewelry/Program.cs
+++ SamJewelry/Program.cs
@@ -9,30 +9,25 @@ namespace SamJewelry
   
     class Program
     {
-        private  string JewelryName;
+       
 
         static void Main(string[] args)
         {
-            //create an instnace of Cart
-
-            Cart cart1 = new Cart();
-
-           
+            
           //create an instance of Jewelry 
             var Jewelry1 = new Jewelry();
           Jewelry1.JeWelryName = "goldJewelry";
             Jewelry1.Typeofjewelry = Jewelrytype.GoldJewelry;
 
-            var Jewelry2 = new Jewelry();
-            Jewelry1.JeWelryName = "silver";
-            Jewelry2.Typeofjewelry = Jewelrytype.silverJewelry;
-       
-            
-            cart1.AddToCart(Jewelry1);
-       // Pick jewelry items and put it cart
+            // Addtocart
+
+            Jewelry1.AddNecklacestocart(2);
+
+
 
 
             // View cart 
+            Jewelry1.viewcart();
 
 
 
